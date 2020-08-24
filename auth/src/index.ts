419a7cbe819e1542e769464a6310872a02e869c8
emreturgutce <emrebursalee@gmail.com>
2020-08-24 19:23:11 +0300

    auth service hello world route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,4 +3,8 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req, res) =>
+  res.json({ message: 'Hello World!' })
+)
+
 app.listen(3000, () => console.log('Auth Service running on port 3000'))

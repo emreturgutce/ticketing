dcf438d155a18fdcbf2c3d12771216c1caf77ef8
emreturgutce <emrebursalee@gmail.com>
2020-08-27 14:24:58 +0300

    user model added

---
+++ auth/src/models/user.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+const User = model('User', userSchema)
+
+export { User }

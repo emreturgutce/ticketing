821eed99023d933cbcb97cbc023316d0a4f0063d
emreturgutce <emrebursalee@gmail.com>
2020-08-27 21:30:00 +0300

    basic signin route implementation and request-validation mw

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,22 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+import { body } from 'express-validator'
+import { validateRequest } from '../middleware/validate-request'
 
 const router = Router()
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hello World')
-})
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send('Hello World')
+  }
+)
 
 export { router as signinRouter }

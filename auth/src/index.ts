ef13c8d345e32b4252647fa3edf2de759c6f40cc
emreturgutce <emrebursalee@gmail.com>
2020-08-27 14:12:49 +0300

    MongoDB service added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express'
 import 'express-async-errors'
+import mongoose from 'mongoose'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signupRouter } from './routes/signup'
 import { signoutRouter } from './routes/signout'
 import { errorHandler } from './middleware/error-handler'
 import { notFound } from './middleware/not-found'
+
+mongoose
+  .connect('mongodb://auth-mongo-srv:27017/auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(`Error occurred connecting MongoDB: ${err}`))
 
 const app = express()
 app.use(express.json())

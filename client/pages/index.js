2ff0f88a0a5489c2ee83b32feb01a1dcf1d79742
emreturgutce <emrebursalee@gmail.com>
2020-08-29 00:32:16 +0300

    fetching data during ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,1 +1,24 @@
-export default () => <h1>Banana 4</h1>
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => <h1>This is 🐱‍👓 Landing Page</h1>
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // When on the Server
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    )
+
+    return data
+  } else {
+    // When on the Browser
+    const { data } = await axios.get('/api/users/currentuser')
+
+    return data
+  }
+}
+
+export default LandingPage

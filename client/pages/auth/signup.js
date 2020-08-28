04922b3fa394d8a0a37102733872e09eca76921a
emreturgutce <emrebursalee@gmail.com>
2020-08-28 23:09:30 +0300

    handling signup form submit event

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,40 @@
-export default () => (
-  <form>
-    <h1>Sign Up</h1>
-    <div className='form-group'>
-      <label>Email Address</label>
-      <input type='text' className='form-control' />
-    </div>
-    <div className='form-group'>
-      <label>Password</label>
-      <input type='text' className='form-control' />
-    </div>
-    <button className='btn btn-primary'>Sign Up</button>
-  </form>
-)
+import { useState } from 'react'
+import axios from 'axios'
+
+export default () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    axios
+      .post('/api/users/signup', { email, password })
+      .then(response => console.log(response.data))
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Sign Up</h1>
+      <div className='form-group'>
+        <label>Email Address</label>
+        <input
+          type='email'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className='form-control'
+        />
+      </div>
+      <div className='form-group'>
+        <label>Password</label>
+        <input
+          type='password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className='form-control'
+        />
+      </div>
+      <button className='btn btn-primary'>Sign Up</button>
+    </form>
+  )
+}

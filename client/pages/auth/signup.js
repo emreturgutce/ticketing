4f6b4e3f80ddd5e3145615f1ed8a70c418204ba7
emreturgutce <emrebursalee@gmail.com>
2020-08-28 23:29:55 +0300

    useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,24 +1,20 @@
 import { useState } from 'react'
 import axios from 'axios'
+import useRequest from '../../hooks/use-request'
 
 export default () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [errors, setErrors] = useState([])
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: { email, password },
+  })
 
-  const handleSubmit = async e => {
+  const handleSubmit = e => {
     e.preventDefault()
 
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      })
-
-      console.log(response)
-    } catch (err) {
-      setErrors(err.response.data.errors)
-    }
+    doRequest()
   }
 
   return (
@@ -42,16 +38,7 @@ export default () => {
           className='form-control'
         />
       </div>
-      {errors.length > 0 && (
-        <div className='alert alert-danger'>
-          <h4>Ooops...</h4>
-          <ul className='my-0'>
-            {errors.map(err => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
   )

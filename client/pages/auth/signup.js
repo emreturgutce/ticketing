2f51e19c739cb571b6e9e2da673b9c15b9952d02
emreturgutce <emrebursalee@gmail.com>
2020-08-28 23:02:13 +0300

    signup form

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,14 @@
+export default () => (
+  <form>
+    <h1>Sign Up</h1>
+    <div className='form-group'>
+      <label>Email Address</label>
+      <input type='text' className='form-control' />
+    </div>
+    <div className='form-group'>
+      <label>Password</label>
+      <input type='text' className='form-control' />
+    </div>
+    <button className='btn btn-primary'>Sign Up</button>
+  </form>
+)

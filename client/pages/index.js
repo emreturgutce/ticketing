478823c27176dfcc8f0b9b2bed5549fb86e42439
emreturgutce <emrebursalee@gmail.com>
2020-08-29 00:49:16 +0300

    LandingPage content

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,17 @@
 import axios from 'axios'
 import buildClient from '../api/build-client'
 
-const LandingPage = ({ currentUser }) => <h1>This is 🐱‍👓 Landing Page</h1>
+const LandingPage = ({ data }) => {
+  return data.currentUser ? (
+    <h1>This is 🐱‍👓 Landing Page</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  )
+}
 
 LandingPage.getInitialProps = async ({ req }) => {
   const { data } = await buildClient({ req }).get('/api/users/currentuser')
-
+  console.log(data)
   return data
 }
 

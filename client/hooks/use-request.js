983551ccc8bd11cedcbf82a664284b09fc2902ee
emreturgutce <emrebursalee@gmail.com>
2020-08-28 23:34:46 +0300

    onSuccess callback

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useState } from 'react'
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null)
 
   const doRequest = async () => {
@@ -9,6 +9,8 @@ export default ({ url, method, body }) => {
       setErrors(null)
 
       const response = await axios[method](url, body)
+
+      if (onSuccess) onSuccess(response.data)
 
       return response.data
     } catch (err) {
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import axios from 'axios'
 import useRequest from '../../hooks/use-request'
+import Router from 'next/router'
 
 export default () => {
   const [email, setEmail] = useState('')
@@ -9,6 +9,7 @@ export default () => {
     url: '/api/users/signup',
     method: 'post',
     body: { email, password },
+    onSuccess: () => Router.push('/'),
   })
 
   const handleSubmit = e => {

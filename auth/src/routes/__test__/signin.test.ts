68f871e6dcf1c885ba349bf32cae39ba262478d8
emreturgutce <emrebursalee@gmail.com>
2020-08-28 14:43:45 +0300

    signin route tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,48 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('returns a 200 on successful signin', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200)
+})
+
+it('returns a 400 when email that does not exist is supplied', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('fails when an incorrect password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'wrongpassword',
+    })
+    .expect(400)
+})

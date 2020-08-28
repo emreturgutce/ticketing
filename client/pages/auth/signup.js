158848b96756768b7c8ec8b3998fc8bf6c0263c1
emreturgutce <emrebursalee@gmail.com>
2020-08-28 23:18:53 +0300

    signup validation

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,13 +4,21 @@ import axios from 'axios'
 export default () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState([])
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
 
-    axios
-      .post('/api/users/signup', { email, password })
-      .then(response => console.log(response.data))
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      })
+
+      console.log(response)
+    } catch (err) {
+      setErrors(err.response.data.errors)
+    }
   }
 
   return (
@@ -34,6 +42,16 @@ export default () => {
           className='form-control'
         />
       </div>
+      {errors.length > 0 && (
+        <div className='alert alert-danger'>
+          <h4>Ooops...</h4>
+          <ul className='my-0'>
+            {errors.map(err => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
   )

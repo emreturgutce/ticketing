b739623b5f862d812abcabf4d252ec7a66ceca87
emreturgutce <emrebursalee@gmail.com>
2020-08-29 11:43:02 +0300

    signin form

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import useRequest from '../../hooks/use-request'
+import Router from 'next/router'
+
+export default () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: { email, password },
+    onSuccess: () => Router.push('/'),
+  })
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    doRequest()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Sign In</h1>
+      <div className='form-group'>
+        <label>Email Address</label>
+        <input
+          type='email'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className='form-control'
+        />
+      </div>
+      <div className='form-group'>
+        <label>Password</label>
+        <input
+          type='password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className='form-control'
+        />
+      </div>
+      {errors}
+      <button className='btn btn-primary'>Sign In</button>
+    </form>
+  )
+}

fde4eab4ce9e1c30dd9f6a8c8327ee5daed6ee39
emreturgutce <emrebursalee@gmail.com>
2020-09-26 11:04:16 +0300

    validating title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,8 +1,17 @@
 import { Router, Request, Response } from 'express'
-import { requireAuth } from '@et-ticketing/common'
+import { requireAuth, validateRequest } from '@et-ticketing/common'
+import { body } from 'express-validator'
 
 const router = Router()
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {})
+router.post('/api/tickets', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater than 0')
+], validateRequest, (req: Request, res: Response) => {})
 
 export { router as createTicketRouter }

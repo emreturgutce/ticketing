75f44ab80a9422a51fd7327806d3b045300b12be
emreturgutce <emrebursalee@gmail.com>
2020-09-26 11:00:06 +0300

    testing title and price

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -25,8 +25,42 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(response.status).not.toEqual(401)
 })
 
-it('returns an error if an invalid title is provided', async () => {})
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title: "",
+      price: 10,
+    })
+    .expect(400)
 
-it('returns an error if an invalid price is provided', async () => {})
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      price: 10,
+    })
+    .expect(400)
+})
+
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title: "asdasd",
+      price: -10,
+    })
+    .expect(400)
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title: "asdasd",
+    })
+    .expect(400)
+})
 
 it('creates a ticket if valid inputs', async () => {})

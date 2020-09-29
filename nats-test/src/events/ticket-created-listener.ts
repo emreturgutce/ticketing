c464e54990214f7a283eb8c5b2e0a5e826243934
emreturgutce <emrebursalee@gmail.com>
2020-09-29 14:21:22 +0300

    readonly keyword added to subject property of TicketCreatedListener class

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,7 +4,7 @@ import { Subjects } from './subjects'
 import { TicketCreatedEvent } from './ticket-created-event'
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated
+  readonly subject = Subjects.TicketCreated
   queueGroupName = 'payments-service'
 
   onMessage(data: any, msg: Message): void {

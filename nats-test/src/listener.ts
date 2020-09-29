ba57592253996903e839fc4514ae7365af6b25a1
emreturgutce <emrebursalee@gmail.com>
2020-09-29 13:18:19 +0300

    cleaning the things that we do not need

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -14,27 +14,6 @@ stan.on('connect', () => {
     console.log('NATS connection closed!')
     process.exit()
   })
-
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service')
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'queue-group-name',
-    options
-  )
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData()
-
-    if (typeof data === 'string')
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-
-    msg.ack()
-  })
 })
 
 process.on('SIGINT', () => stan.close())
@@ -82,5 +61,16 @@ abstract class Listener {
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf8'))
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created'
+  queueGroupName = 'payments-service'
+
+  onMessage(data: any, msg: Message): void {
+    console.log('Event data!: ', data)
+
+    msg.ack()
   }
 }

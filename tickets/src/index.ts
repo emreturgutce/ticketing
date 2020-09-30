0430d95815b2fcd7195ef8d2c685333dc9dbc74c
emreturgutce <emrebursalee@gmail.com>
2020-09-30 11:53:53 +0300

    connected to NatsWrapper

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,13 +1,25 @@
 import { app } from './app'
 import mongoose from 'mongoose'
+import { natsWrapper } from './nats-wrapper'
+import { randomBytes } from 'crypto'
 
-mongoose
-  .connect(process.env.MONGO_URI!, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log(`Error occurred connecting MongoDB: ${err}`))
+async function main() {
+  await natsWrapper.connect(
+    'ticketing',
+    randomBytes(4).toString('hex'),
+    'http://nats-srv:4222'
+  )
 
-app.listen(3000, () => console.log('Tickets Service running on port 3000'))
+  mongoose
+    .connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(`Error occurred connecting MongoDB: ${err}`))
+
+  app.listen(3000, () => console.log('Tickets Service running on port 3000'))
+}
+
+main()

8558a97c0586d72768016cab6ffba4832a849358
emreturgutce <emrebursalee@gmail.com>
2020-09-30 11:54:54 +0300

    creating an instance of TicketCreatedPublisher and publishing events

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,29 +2,41 @@ import { Router, Request, Response } from 'express'
 import { requireAuth, validateRequest } from '@et-ticketing/common'
 import { body } from 'express-validator'
 import { Ticket } from '../models/ticket'
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = Router()
 
-router.post('/api/tickets', requireAuth, [
-  body('title')
-    .not()
-    .isEmpty()
-    .withMessage('Title is required'),
-  body('price')
-    .isFloat({ gt: 0 })
-    .withMessage('Price must be greater than 0')
-], validateRequest, async (req: Request, res: Response) => {
-  const { title, price } = req.body
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body
 
-  const ticket = Ticket.build({
-    title,
-    price,
-    userId: req.currentUser!.id
-  })
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    })
 
-  await ticket.save()
+    await ticket.save()
 
-  res.status(201).json({ ticket })
-})
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    })
+
+    res.status(201).json({ ticket })
+  }
+)
 
 export { router as createTicketRouter }

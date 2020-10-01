c91501994000ba357ab15c66801d9cfc680b934f
emreturgutce <emrebursalee@gmail.com>
2020-10-01 17:30:23 +0300

    mongodb-memory-server version changed to 6.5.1 and some typos fixed

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,7 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
-import { app } from '../app'
-import request from 'supertest'
 import jwt from 'jsonwebtoken'
 
 let mongo: MongoMemoryServer
@@ -14,7 +12,7 @@ declare global {
   }
 }
 
-jest.mock('../../nats-wrapper')
+jest.mock('../nats-wrapper')
 
 beforeAll(async () => {
   mongo = new MongoMemoryServer()
@@ -22,17 +20,17 @@ beforeAll(async () => {
   process.env.JWT_KEY =
     'ba25ecf355069f0aa3f20e8a4cbc5f4d706bd46adec52effd28d67a060fb8876'
 
-  mongoose
-    .connect(mongoUri, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.log(`Error occurred connecting MongoDB: ${err}`))
+  await mongoose.connect(mongoUri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  console.log('Connected to MongoDB')
 })
 
 beforeEach(async () => {
+  jest.clearAllMocks()
+
   const collections = await mongoose.connection.db.collections()
 
   collections.forEach(async collection => await collection.deleteMany({}))

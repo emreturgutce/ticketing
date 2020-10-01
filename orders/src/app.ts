7f556f633cd8669a2bc0430f89d2921c2e1f342a
emreturgutce <emrebursalee@gmail.com>
2020-10-01 12:51:55 +0300

    orders project created

---
+++ orders/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import 'express-async-errors'
+import cookieSession from 'cookie-session'
+import { errorHandler, notFound, currentUser } from '@et-ticketing/common'
+import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
+import { updateTicketRouter } from './routes/update'
+import { indexTicketRouter } from './routes/index'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(express.json())
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' })
+)
+app.use(currentUser)
+
+app.use(createTicketRouter)
+app.use(showTicketRouter)
+app.use(indexTicketRouter)
+app.use(updateTicketRouter)
+
+app.use(notFound)
+app.use(errorHandler)
+
+export { app }

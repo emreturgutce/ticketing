4dbc42043c894f4330dbd72b43fcebad61757f1e
emreturgutce <emrebursalee@gmail.com>
2020-10-01 18:03:18 +0300

    showOrderRouter implemented

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,37 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@et-ticketing/common'
 import { Router, Request, Response } from 'express'
+import { body } from 'express-validator'
+import { Order } from '../models/order'
+import mongoose from 'mongoose'
 
 const router = Router()
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({})
-})
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  [
+    body('orderId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('orderId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket')
+
+    if (!order) throw new NotFoundError()
+
+    // Making sure user really owns the order
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError()
+
+    res.json({ order })
+  }
+)
 
 export { router as showOrderRouter }

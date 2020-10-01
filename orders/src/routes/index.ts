a07356069f14d7ab1dad4df1d18cf6d6447643f3
emreturgutce <emrebursalee@gmail.com>
2020-10-01 17:39:04 +0300

    index route handler implemented

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
+import { requireAuth } from '@et-ticketing/common'
 import { Router, Request, Response } from 'express'
+import { Order } from '../models/order'
 
 const router = Router()
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  res.send({})
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({ userId: req.currentUser!.id }).populate(
+    'ticket'
+  )
+
+  res.json({ orders })
 })
 
 export { router as indexOrderRouter }

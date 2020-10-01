ddd21c9742953d1aedb138843de47511f7d01e08
emreturgutce <emrebursalee@gmail.com>
2020-10-01 16:24:03 +0300

    newOrderRouter implemented

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose'
 import { Router, Request, Response } from 'express'
-import { requireAuth, validateRequest } from '@et-ticketing/common'
+import {
+  BadRequestError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@et-ticketing/common'
 import { body } from 'express-validator'
+import { Ticket } from '../models/ticket'
+import { Order, OrderStatus } from '../models/order'
 
 const router = Router()
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60
 
 router.post(
   '/api/orders',
@@ -17,7 +26,32 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({})
+    const { ticketId } = req.body
+
+    // Finding ticket with the given id and making sure it exists
+    const ticket = await Ticket.findById(ticketId)
+
+    if (!ticket) throw new NotFoundError()
+
+    // Making sure the ticket with the given id is not reserved
+    const isReserved = ticket.isReserved()
+
+    if (isReserved) throw new BadRequestError('Ticket is already reserved')
+
+    // Calculating expiration date
+    const expiration = new Date()
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    })
+
+    await order.save()
+
+    res.status(201).json({ order })
   }
 )
 

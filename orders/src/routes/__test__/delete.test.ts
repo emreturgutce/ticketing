ae1a75b4bf76e211827b5445bfff947ed7403fea
emreturgutce <emrebursalee@gmail.com>
2020-10-01 18:50:50 +0300

    delete route handler test implemented

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { Order, OrderStatus } from '../../models/order'
+import { Ticket } from '../../models/ticket'
+
+it('marks an order as cancelled', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  })
+
+  await ticket.save()
+
+  const user = global.signup()
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  await request(app)
+    .delete(`/api/orders/${order.order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204)
+
+  const updatedOrder = await Order.findById(order.order.id)
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
+})

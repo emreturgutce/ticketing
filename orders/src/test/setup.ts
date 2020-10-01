03dba87a609f97ee2b975da9ecf028cc04b2f058
emreturgutce <emrebursalee@gmail.com>
2020-10-01 17:30:40 +0300

    mongodb-memory-server version changed to 6.5.1 and some typos fixed

--- orders/src/test/setup.ts
+++ orders/src/test/setup.ts
@@ -12,7 +12,7 @@ declare global {
   }
 }
 
-jest.mock('../../nats-wrapper')
+jest.mock('../nats-wrapper')
 
 beforeAll(async () => {
   mongo = new MongoMemoryServer()
@@ -20,20 +20,23 @@ beforeAll(async () => {
   process.env.JWT_KEY =
     'ba25ecf355069f0aa3f20e8a4cbc5f4d706bd46adec52effd28d67a060fb8876'
 
-  mongoose
-    .connect(mongoUri, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.log(`Error occurred connecting MongoDB: ${err}`))
+  await mongoose.connect(mongoUri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+
+  console.log('Connected to MongoDB')
 })
 
 beforeEach(async () => {
+  jest.clearAllMocks()
+
   const collections = await mongoose.connection.db.collections()
 
-  collections.forEach(async collection => await collection.deleteMany({}))
+  collections.forEach(async collection => {
+    await collection.deleteMany({})
+  })
 })
 
 afterAll(async () => {

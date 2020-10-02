4b95c75ee0cb103b87e40d82b46ef4f58951df32
emreturgutce <emrebursalee@gmail.com>
2020-10-02 19:25:17 +0300

    publishing order creation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -9,6 +9,8 @@ import {
 import { body } from 'express-validator'
 import { Ticket } from '../models/ticket'
 import { Order, OrderStatus } from '../models/order'
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = Router()
 
@@ -50,6 +52,17 @@ router.post(
     })
 
     await order.save()
+
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price,
+      },
+    })
 
     res.status(201).json({ order })
   }

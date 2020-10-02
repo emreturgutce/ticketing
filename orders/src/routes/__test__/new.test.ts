558a08673c9f0015dcf06505a8bc24590e0061d9
emreturgutce <emrebursalee@gmail.com>
2020-10-02 19:31:11 +0300

    testing order creation event

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest'
 import { app } from '../../app'
 import { Order, OrderStatus } from '../../models/order'
 import { Ticket } from '../../models/ticket'
+import { natsWrapper } from '../../__mocks__/nats-wrapper'
 
 it('returns an error if the ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId()
@@ -51,4 +52,21 @@ it('reserves a ticket', async () => {
     .set('Cookie', global.signup())
     .send({ ticketId: ticket.id })
     .expect(201)
+})
+
+it('emits an order created event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  })
+
+  await ticket.save()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
 })

552f06e816bddf690f22d208cc5917b895bbb6d0
emreturgutce <emrebursalee@gmail.com>
2020-10-02 20:23:09 +0300

    ticket updated listener implemented

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,23 @@
+import { Listener, Subjects, TicketUpdatedEvent } from '@et-ticketing/common'
+import { queueGroupName } from './queue-group-name'
+import { Message } from 'node-nats-streaming'
+import { Ticket } from '../../models/ticket'
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject = Subjects.TicketUpdated
+  queueGroupName = queueGroupName
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.id)
+
+    if (!ticket) throw new Error('Ticket not found')
+
+    const { title, price } = data
+
+    ticket.set({ title, price })
+
+    await ticket.save()
+
+    msg.ack()
+  }
+}

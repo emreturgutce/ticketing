1b6d2fa7a564f483d2800d3ea8df772f54cf8029
emreturgutce <emrebursalee@gmail.com>
2020-10-03 12:49:59 +0300

    ticket test implemented

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,28 @@
+import { Ticket } from '../ticket'
+
+it('implements optimistic concurrency control', async done => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  })
+
+  await ticket.save()
+
+  const firstInstance = await Ticket.findById(ticket.id)
+
+  const secondInstance = await Ticket.findById(ticket.id)
+
+  firstInstance!.set({ price: 10 })
+  secondInstance!.set({ price: 15 })
+
+  await firstInstance!.save()
+
+  try {
+    await secondInstance!.save()
+  } catch (err) {
+    return done()
+  }
+
+  throw new Error('Should not react this point')
+})

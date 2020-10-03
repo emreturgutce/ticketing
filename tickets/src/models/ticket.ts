e5de393b1a49213e149dc0418531a9264c4e82ab
emreturgutce <emrebursalee@gmail.com>
2020-10-03 11:57:33 +0300

    adding concurrency control to our ticket model

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,42 +1,51 @@
 import { Document, model, Model, Schema } from 'mongoose'
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
 
 interface TicketAttrs {
-  title: string;
-  price: number;
-  userId: string;
+  title: string
+  price: number
+  userId: string
 }
 
 interface TicketDoc extends Document {
-  title: string;
-  price: number;
-  userId: string;
+  title: string
+  price: number
+  userId: string
+  version: number
 }
 
 interface TicketModel extends Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc
 }
 
-const ticketSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const ticketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true,
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+      },
+    },
   }
-}, {
-  toJSON: {
-    transform(doc, ret) {
-      ret.id = ret._id
-      delete ret._id
-    }
-  }
-})
+)
+
+ticketSchema.set('versionKey', 'version')
+
+ticketSchema.plugin(updateIfCurrentPlugin)
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => new Ticket(attrs)
 

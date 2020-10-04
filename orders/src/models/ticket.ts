f700e3986ab7ecb25de269d028f528a36d271042
emreturgutce <emrebursalee@gmail.com>
2020-10-04 14:38:17 +0300

    version control added to ticket model in orders service

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,5 +1,6 @@
 import { Document, Model, Schema, model } from 'mongoose'
 import { Order, OrderStatus } from './order'
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
 
 interface TicketAttrs {
   id: string
@@ -10,6 +11,7 @@ interface TicketAttrs {
 export interface TicketDoc extends Document {
   title: string
   price: number
+  version: number
   isReserved(): Promise<boolean>
 }
 
@@ -38,6 +40,9 @@ const ticketSchema = new Schema(
     },
   }
 )
+
+ticketSchema.set('versionKey', 'version')
+ticketSchema.plugin(updateIfCurrentPlugin)
 
 ticketSchema.statics.build = (attrs: TicketAttrs) =>
   new Ticket({ _id: attrs.id, ...attrs })

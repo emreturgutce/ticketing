100a7a4bb4d6acc855d52e065440e573f317c490
emreturgutce <emrebursalee@gmail.com>
2020-10-05 15:35:12 +0300

    testing ticket-created-listener

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+import { Message } from 'node-nats-streaming'
+import { TicketCreatedEvent } from '@et-ticketing/common'
+import { natsWrapper } from '../../../nats-wrapper'
+import { TicketCreatedListener } from '../ticket-created-listener'
+import { Ticket } from '../../../models/ticket'
+
+const setup = async () => {
+  const listener = new TicketCreatedListener(natsWrapper.client)
+
+  const data: TicketCreatedEvent['data'] = {
+    version: 0,
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 10,
+    userId: mongoose.Types.ObjectId().toHexString(),
+  }
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  }
+
+  return { listener, data, msg }
+}
+
+it('creates and saves a ticket', async () => {
+  const { listener, data, msg } = await setup()
+
+  await listener.onMessage(data, msg)
+
+  const ticket = await Ticket.findById(data.id)
+
+  expect(ticket).toBeDefined()
+  expect(ticket!.title).toEqual(data.title)
+  expect(ticket!.price).toEqual(data.price)
+})
+
+it('acks the message', async () => {})

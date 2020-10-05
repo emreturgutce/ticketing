c1d4aa2088b58658ea57f1d90291edff70aa6f65
emreturgutce <emrebursalee@gmail.com>
2020-10-05 20:34:35 +0300

    testing: rejects updates if the ticket is reserved

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { app } from '../../app'
 import mongoose from 'mongoose'
 import { natsWrapper } from '../../nats-wrapper'
+import { Ticket } from '../../models/ticket'
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString()
@@ -114,4 +115,31 @@ it('publishes an event', async () => {
   })
 
   expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
+
+it('rejects updates if the ticket is reserved', async () => {
+  const cookie = global.signup()
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'asdasd',
+      price: 20,
+    })
+
+  const ticket = await Ticket.findById(response.body.ticket.id)
+
+  ticket!.set({ orderId: mongoose.Types.ObjectId().toHexString() })
+
+  await ticket!.save()
+
+  await request(app)
+    .put(`/api/tickets/${response.body.ticket.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100,
+    })
+    .expect(400)
 })

9ee582d6d5601ab3ec11b6844613d638940f2be1
emreturgutce <emrebursalee@gmail.com>
2020-10-05 19:40:24 +0300

    publishing ticket updated event inside of order created listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import { Message } from 'node-nats-streaming'
 import { Listener, OrderCreatedEvent, Subjects } from '@et-ticketing/common'
 import { queueGroupName } from './queue-group-name'
 import { Ticket } from '../../models/ticket'
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated
@@ -15,6 +16,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     ticket.set({ orderId: data.id })
 
     await ticket.save()
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    })
 
     msg.ack()
   }

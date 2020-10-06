e0a87cd91d23451f997d46a232d3faef5555d0bc
emreturgutce <emrebursalee@gmail.com>
2020-10-06 21:32:40 +0300

    OrderCreatedListener for payments service

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, OrderCreatedEvent, Subjects } from '@et-ticketing/common'
+import { queueGroupName } from './queue-group-name'
+import { Order } from '../../models/order'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated
+  queueGroupName = queueGroupName
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    })
+
+    await order.save()
+
+    msg.ack()
+  }
+}

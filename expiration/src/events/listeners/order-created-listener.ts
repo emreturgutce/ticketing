fbb69ba7cf40c3547d2091504935204520688aea
emreturgutce <emrebursalee@gmail.com>
2020-10-06 12:40:58 +0300

    queueing jobs

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,15 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@et-ticketing/common'
 import { Message } from 'node-nats-streaming'
+import { expirationQueue } from '../../queues/expiration-queue'
 import { queueGroupName } from './queue-group-name'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated
   queueGroupName = queueGroupName
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    })
+  }
 }

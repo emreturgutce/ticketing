b04cdfbbd31e10a5c15e9f02dd9ada0c7fa4626c
emreturgutce <emrebursalee@gmail.com>
2020-10-06 17:58:23 +0300

    order model for payments service

---
+++ payments/src/models/order.ts
@@ -0,0 +1,65 @@
+import { OrderStatus } from '@et-ticketing/common'
+import { Document, model, Model, Schema } from 'mongoose'
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
+
+interface OrderAttrs {
+  id: string
+  version: number
+  userId: string
+  price: number
+  status: OrderStatus
+}
+
+interface OrderDoc extends Document {
+  version: number
+  userId: string
+  price: number
+  status: OrderStatus
+}
+
+interface OrderModel extends Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc
+}
+
+const orderSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+      },
+    },
+  }
+)
+
+orderSchema.set('versionKey', 'version')
+
+orderSchema.plugin(updateIfCurrentPlugin)
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.version,
+    userId: attrs.userId,
+    status: attrs.status,
+  })
+}
+
+const Order = model<OrderDoc, OrderModel>('order', orderSchema)
+
+export { Order }

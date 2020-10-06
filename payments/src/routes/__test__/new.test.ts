84ae149aae0e85bf4220f585bfd87df0702cbfc1
emreturgutce <emrebursalee@gmail.com>
2020-10-06 23:54:49 +0300

    testing createChargeRouter with valid inputs

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import mongoose from 'mongoose'
 import request from 'supertest'
 import { app } from '../../app'
 import { Order } from '../../models/order'
+import { stripe } from '../../stripe'
+
+jest.mock('../../stripe')
 
 it('returns a 404 if the order does not exist', async () => {
   await request(app)
@@ -57,4 +60,33 @@ it('returns a 400 if the order is cancelled', async () => {
       orderId: order.id,
     })
     .expect(400)
+})
+
+it('returns a 204 with valid inputs', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString()
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    price: 10,
+    status: OrderStatus.Created,
+    userId,
+    version: 0,
+  })
+
+  await order.save()
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signup(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201)
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0]
+
+  expect(chargeOptions.source).toEqual('tok_visa')
+  expect(chargeOptions.amount).toEqual(order.price * 100)
+  expect(chargeOptions.currency).toEqual('usd')
 })

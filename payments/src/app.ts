cafc91390d35af1bdbba6e35e363fe03ec5f6f48
emreturgutce <emrebursalee@gmail.com>
2020-10-06 22:46:52 +0300

    using createChargeRouter

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import cookieSession from 'cookie-session'
 import { errorHandler, notFound, currentUser } from '@et-ticketing/common'
+import { createChargeRouter } from './routes/new'
 
 const app = express()
 app.set('trust proxy', true)
@@ -10,6 +11,8 @@ app.use(
   cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' })
 )
 app.use(currentUser)
+
+app.use(createChargeRouter)
 
 app.use(notFound)
 app.use(errorHandler)

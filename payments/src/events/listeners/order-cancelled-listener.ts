eb3cf15e82a784f54ac525acae04c9eb74f38fa3
emreturgutce <emrebursalee@gmail.com>
2020-10-06 22:15:21 +0300

    OrderCancelledListener implemented

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,29 @@
+import { Message } from 'node-nats-streaming'
+import {
+  Listener,
+  OrderCancelledEvent,
+  OrderStatus,
+  Subjects,
+} from '@et-ticketing/common'
+import { queueGroupName } from '../queue-group-name'
+import { Order } from '../../../models/order'
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled
+  queueGroupName = queueGroupName
+
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    const order = await Order.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    })
+
+    if (!order) throw new Error('Order not found')
+
+    order.set({ status: OrderStatus.Cancelled })
+
+    await order.save()
+
+    msg.ack()
+  }
+}

3b4c0fe2462fdbaa71aa29423bdea03cfcf03da1
emreturgutce <emrebursalee@gmail.com>
2020-10-06 12:41:10 +0300

    listening for order created events

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,3 +1,4 @@
+import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { natsWrapper } from './nats-wrapper'
 
 async function main() {
@@ -22,6 +23,8 @@ async function main() {
 
     process.on('SIGINT', () => natsWrapper.client.close())
     process.on('SIGTERM', () => natsWrapper.client.close())
+
+    new OrderCreatedListener(natsWrapper.client).listen()
   } catch (err) {
     console.error(err)
   }

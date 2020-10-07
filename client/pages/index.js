28870b2074b9108841e1aff0b04c1f2c2a570f5f
emreturgutce <emrebursalee@gmail.com>
2020-10-07 23:14:00 +0300

    a bit clean up

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,13 @@
-import buildClient from '../api/build-client'
-
-const LandingPage = ({ data }) => {
-  return data.currentUser ? (
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
     <h1>You are signed in 🚀</h1>
   ) : (
     <h1>You are NOT signed in</h1>
   )
 }
 
-LandingPage.getInitialProps = async context => {
-  console.log('LANDING PAGE!')
-  const client = buildClient(context)
-  const { data } = await client.get('/api/users/currentuser')
-
-  return data
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {}
 }
 
 export default LandingPage

2ef115af010f53f4f3c84f465cfaee8f9c7b6722
emreturgutce <emrebursalee@gmail.com>
2020-10-07 13:39:35 +0300

    PaymentCreatedListener implemented

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,26 @@
+import { Message } from 'node-nats-streaming'
+import {
+  Listener,
+  OrderStatus,
+  PaymentCreatedEvent,
+  Subjects,
+} from '@et-ticketing/common'
+import { queueGroupName } from './queue-group-name'
+import { Order } from '../../models/order'
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+  readonly subject = Subjects.PaymentCreated
+  queueGroupName = queueGroupName
+
+  async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId)
+
+    if (!order) throw new Error('Order not found')
+
+    order.set({ status: OrderStatus.Complete })
+
+    await order.save()
+
+    msg.ack()
+  }
+}

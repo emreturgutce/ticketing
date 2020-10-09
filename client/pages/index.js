b3a5d69017e6af1bd34e0a58ee15d381a124381a
emreturgutce <emrebursalee@gmail.com>
2020-10-09 11:21:42 +0300

    listing all tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,31 @@
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in 🚀</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const renderTickets = tickets.map(ticket => (
+    <tr key={ticket.id}>
+      <td>{ticket.title}</td>
+      <td>{ticket.price}</td>
+    </tr>
+  ))
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{renderTickets}</tbody>
+      </table>
+    </div>
   )
 }
 
-LandingPage.getInitialProps = async (context, client, currentUser) => {}
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets')
+
+  return { tickets: data.tickets }
+}
 
 export default LandingPage

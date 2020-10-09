10db00ea3489c2616382a3667c5cd9850003ac94
emreturgutce <emrebursalee@gmail.com>
2020-10-09 11:48:48 +0300

    creating an order

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,3 +1,33 @@
-const TicketShow = () => <div>Ticket Show</div>
+import useRequest from '../../hooks/use-request'
+
+const TicketShow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: order => console.log(order),
+  })
+
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={doRequest} className='btn btn-primary'>
+        Purchase
+      </button>
+    </div>
+  )
+}
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query
+
+  const { data } = await client.get(`/api/tickets/${ticketId}`)
+
+  return { ticket: data.ticket }
+}
 
 export default TicketShow

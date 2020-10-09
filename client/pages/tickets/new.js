54e38f1a943a14b6481112ce177108c7e6f0a4a7
emreturgutce <emrebursalee@gmail.com>
2020-10-09 11:03:32 +0300

    form state and handling some events

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,20 +1,44 @@
-const NewTicket = () => (
-  <div>
-    <h1>Create a Ticket</h1>
-    <form>
-      <div className='form-group'>
-        <label>Title</label>
-        <input type='text' className='form-control' />
-      </div>
-      <div className='form-group'>
-        <label>Title</label>
-        <input type='text' className='form-control' />
-      </div>
-      <button type='submit' className='btn btn-primary'>
-        Submit
-      </button>
-    </form>
-  </div>
-)
+import { useState } from 'react'
+
+const NewTicket = () => {
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState('')
+
+  const handleBlur = () => {
+    const value = parseFloat(price)
+
+    if (isNaN(value)) return
+
+    setPrice(value.toFixed(2))
+  }
+
+  return (
+    <div>
+      <h1>Create a Ticket</h1>
+      <form>
+        <div className='form-group'>
+          <label>Title</label>
+          <input
+            className='form-control'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </div>
+        <div className='form-group'>
+          <label>Price</label>
+          <input
+            className='form-control'
+            value={price}
+            onChange={e => setPrice(e.target.value)}
+            onBlur={handleBlur}
+          />
+        </div>
+        <button type='submit' className='btn btn-primary'>
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
 
 export default NewTicket
